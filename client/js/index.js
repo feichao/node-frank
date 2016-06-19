0b8b36960b4c8a812542f2e4b5ed2fd5f3e470bf
feichao <len.may@foxmail.com>
2016-06-19 12:01:49 +0800

    update: index page add shortcut articles

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,27 @@
-var b = 'b';
+'use strict';
 
-module.exports = {
-	b: b
-};
+var indexPage;
+var tabList;
+var articleList;
+
+$(document).ready(function() {
+  indexPage = $('#index-page-wrap');
+  tabList = indexPage.find('.shortcut-articles > ul > li');
+  articleList = indexPage.find('.articles-list');
+
+  tabList.on('click', showArticle);
+
+});
+
+function showArticle(event) {
+  var $this = $(this);
+  var index = tabList.index($this);
+
+  tabList.removeClass('active');
+  $this.addClass('active');
+
+  articleList.removeClass(function(index, css) {
+    return (css.match(/(^|\s)trans-\S+/g) || []).join(' ');
+  }).addClass('trans-' + index);
+
+}
